383bd25493b35b752cd6f9161d1b938d9b02952f
Antonio José Masiá <dev@ajmasia.me>
2024-02-09 22:51:21 +0100

    update viserion kitty settings

--- homes/ajmasia/viserion/programs/kitty/default.nix
+++ homes/ajmasia/viserion/programs/kitty/default.nix
@@ -8,12 +8,13 @@
     font.size = 13;
 
     theme = "Catppuccin-Macchiato";
+    shellIntegration.enableBashIntegration = true;
+    shellIntegration.mode = "no-cursor";
 
     settings = {
       window_padding_width = 6;
       confirm_os_window_close = 0;
       cursor_shape = "underline";
-      shell_integration = "enabled no-cursor";
       detect_urls = "yes";
       copy_on_selects = "yes";
     };
