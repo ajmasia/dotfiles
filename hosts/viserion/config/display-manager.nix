23d57fca3979cb4f6365284fa2acbb204c75558e
Antonio José Masiá <dev@ajmasia.me>
2024-02-13 21:04:27 +0100

    viserion: add sddm display-manager

--- hosts/viserion/config/display-manager.nix
+++ hosts/viserion/config/display-manager.nix
@@ -1,16 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  services = {
-    xserver = {
+  services.xserver = {
+    enable = true;
+
+    xkb.layout = "us";
+    xkb.variant = "altgr-intl";
+    libinput.enable = true;
+
+    displayManager.sddm = {
       enable = true;
 
-      displayManager = {
-        gdm = {
-          enable = true;
-          wayland = true;
-        };
-      };
+      wayland.enable = true;
+      autoNumlock = true;
+      theme = "tokyo-night-sddm";
     };
   };
+
+  environment.systemPackages =
+    let
+      tokyo-night = pkgs.libsForQt5.callPackage ../pkgs/sddm-tokyo-night.nix { };
+    in
+    with pkgs; [
+      tokyo-night # tokyo-night-sddm
+      libsForQt5.qt5.qtgraphicaleffects
+    ];
 }
